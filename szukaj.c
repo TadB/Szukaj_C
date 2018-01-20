7eb174e87d1ff16e6ac92e9effcac519dea618a5
TadB <tadeusz.brzytwa@gmail.com>
2018-01-20 16:07:43 +0100

    wersja przed testowa

--- szukaj.c
+++ szukaj.c
@@ -1,6 +1,25 @@
 #include "szukaj.h"
 #include "exponent.h"
 
+int find(char *sentence, char *search){
+    char *it;
+    int i, len, fingerprint, origin;
+    long int hBufor=0;
+
+    it = sentence;
+    len = length(search);
+    origin = hash(search, len, hBufor); //obliczenie fingerprinta szukanego ciagu znakow
+    hBufor=0;
+    for(i=0;(*(it+len))!='\n';it++, i++){
+        fingerprint = hash(it, len, hBufor);
+        if(fingerprint == origin){
+            if(check(it, search))
+                return(i);
+        }
+    }
+
+
+}
 
 //&bMod - adres zmiennej w ktorej przechowuje prawie obliczony hash //przed operacja modulo
 //*sequence - adres poczatku (pierwszy wyraz) sprawdzanego fragmentu ciagu
@@ -33,6 +52,18 @@ int hash(char *sequence, int howMany, long int* bMod){
     *bMod = sum;
     result = sum%modulo;
     return(result);
+}
+
+int length(char *input){
+    char *it;
+    int counter;
+
+    it = input;
+    for(it=input; (*it)!='\n'; it++){
+        counter++;
+    }
+    return(counter);
+
 }
 
 bool check(char *suspect, char *word){
--- szukaj.h
+++ szukaj.h
@@ -5,6 +5,6 @@
 int hash(char *, int, int); //funkcja hashujaca - oblicza "wartosc" podanego slowa wg algorytmu
 // bool check(char *); //pierwsze sprawdzenie, jesli nie przejdzie brak zgodnosci/ jesli zwroci true - podejrzenie o zgodnosc
 bool check(char *, char *); //drugie sprawdzenie, malo efektywne ale wykonywane nie wiele razy, maly wplyw na zlozonosc obliczeniowa algorytmu
-
-char *find(char *, char *); //glowna funkcja szukajaca podanego ciagu znakow w podanym zdaniu (przesylany adres zdania i szukanego ciagu)
+int length(char *); //zwraca dlugosc ciagu znakow
+int find(char *, char *); //glowna funkcja szukajaca podanego ciagu znakow w podanym zdaniu (przesylany adres zdania i szukanego ciagu)
 #endif
